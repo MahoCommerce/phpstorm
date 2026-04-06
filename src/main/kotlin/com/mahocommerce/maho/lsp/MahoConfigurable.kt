c2e1f93769668e59f3bc455d7586dfe36d866ef1
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-04-06 14:19:51 +0200

    Fixed nullable binding in settings panel

--- src/main/kotlin/com/mahocommerce/maho/lsp/MahoConfigurable.kt
+++ src/main/kotlin/com/mahocommerce/maho/lsp/MahoConfigurable.kt
@@ -3,19 +3,27 @@ package com.mahocommerce.maho.lsp
 import com.intellij.openapi.options.BoundConfigurable
 import com.intellij.openapi.project.Project
 import com.intellij.platform.lsp.api.LspServerManager
-import com.intellij.ui.dsl.builder.bindText
 import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.dsl.builder.bindText
+import com.intellij.ui.dsl.builder.AlignX
 
 class MahoConfigurable(private val project: Project) : BoundConfigurable("Maho") {
 
     private val settings get() = MahoSettings.getInstance(project)
 
+    private var phpCommand: String
+        get() = settings.state.phpCommand ?: "php"
+        set(value) { settings.state.phpCommand = value }
+
     override fun createPanel() = panel {
         group("LSP Server") {
             row("PHP command:") {
                 textField()
-                    .bindText(settings.state::phpCommand)
-                    .comment("How to invoke PHP (e.g. <code>php</code>, <code>/usr/local/bin/php</code>, <code>docker exec mycontainer php</code>)")
+                    .align(AlignX.FILL)
+                    .bindText(::phpCommand)
+            }
+            row("") {
+                text("How to invoke PHP, e.g. <code>php</code>, <code>/usr/local/bin/php</code>, or <code>docker exec mycontainer php</code>")
             }
         }
     }
